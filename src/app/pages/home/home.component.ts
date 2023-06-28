5f4a21d448517526306b0af23b1f6128ceabfb2e
Abu Suraj <abusurajbd@gmail.com>
2023-06-28 13:12:19 +0600

    add product-box component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+const ROWS_HEIGHT:{[id:number]:number} = {1:400, 3:335, 4: 350};
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -8,12 +9,14 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   cols = 3;
   category: string | undefined;
+  rowHeight = ROWS_HEIGHT[this.cols];
 
   ngOnInit(): void {
     
   }
   onColumnsCountChange(cols:number): void{
     this.cols = cols;
+    this.rowHeight = ROWS_HEIGHT[this.cols];
   }
 
  

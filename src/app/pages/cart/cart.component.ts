da2c35d34aa05a3d4d80da86ce1f7987a622041f
Abu Suraj <abusurajbd@gmail.com>
2023-06-29 20:38:29 +0600

    added no product div

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Cart, CartItem } from 'src/app/Models/cart.model';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.less']
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
+  cart: Cart ={items:[{
+    id: 1,
+    name: 'Item 1',
+    price: 100,
+    quantity: 1,
+    product:'https://via.placehold.com/150'
+  }]};
 
+  dataSource: CartItem [] = [];
+  displayedColumns: string[] = ['product','name', 'price', 'quantity', 'total', 'action'];
+  
+  ngOnInit(): void {
+    this.dataSource = this.cart.items;
+  }
+   
 }

efefd73776d41e64900ae16388d096eedb3f98b9
Abu Suraj <abusurajbd@gmail.com>
2023-07-01 14:24:36 +0600

    back to old approach

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -9,33 +9,33 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   itemsQuantity = 0;
-  cart: Cart = { items: [] };
-  // private _cart: Cart = {items: []}
+  // cart: Cart = { items: [] };
+  private _cart: Cart = {items: []}
 
-  // @Input()
-  // get cart():Cart {
-  //   return this._cart;
-  // }
+  @Input()
+  get cart():Cart {
+    return this._cart;
+  }
 
-  // set cart(cart: Cart) {
-  //   this._cart = cart;
+  set cart(cart: Cart) {
+    this._cart = cart;
 
-  //   this.itemsQuantity = cart.items
-  //   .map(item =>item.quantity)
-  //   .reduce((prev,current) => prev+ current, 0);
-  // }
+    this.itemsQuantity = cart.items
+    .map(item =>item.quantity)
+    .reduce((prev,current) => prev+ current, 0);
+  }
 
   constructor(private cartService: CartService) {}
 
   ngOnInit(): void {
-    this.cartService.cart.subscribe((_cart) => {
-      this.cart = _cart;
+    // this.cartService.cart.subscribe((_cart) => {
+    //   this.cart = _cart;
 
-      this.itemsQuantity = _cart.items
-        .map((item) => item.quantity)
-        .reduce((prev, current) => prev + current, 0);
-    });
-    console.log(this.cart)
+    //   this.itemsQuantity = _cart.items
+    //     .map((item) => item.quantity)
+    //     .reduce((prev, current) => prev + current, 0);
+    // });
+    // console.log(this.cart)
   }
 
   getTotal(items: CartItem[]): number {

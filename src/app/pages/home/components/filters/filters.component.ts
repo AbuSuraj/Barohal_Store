7ef609b2de6d47484117a4c421edeedd3072a7b6
Abu Suraj <abusurajbd@gmail.com>
2023-07-23 22:02:27 +0600

    add some comments

--- src/app/pages/home/components/filters/filters.component.ts
+++ src/app/pages/home/components/filters/filters.component.ts
@@ -33,3 +33,12 @@ export class FiltersComponent implements OnInit, OnDestroy {
     this.categoriesSubscription.unsubscribe();
   }
 }
+
+
+// Angular Development: Designing and implementing features for web applications using Angular, ensuring seamless user experiences.
+// Feature Creation: Leading the conceptualization and development of new features for applications.
+// CRUD Operations: Proficiently performing Create, Read, Update, and Delete operations for data management.
+// Bug Fixing and Quality Assurance: Identifying and resolving software defects to ensure a smooth user experience.
+// Agile Development: Embracing agile methodologies for adaptive project delivery.
+// Collaboration and Communication: Working closely with cross-functional teams to foster a collaborative environment.
+// Continuous Learning: Keeping up-to-date with the latest advancements in Angular and web development.
